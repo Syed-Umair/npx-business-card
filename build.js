618b34c41051b5defb15edcf00ecaba682a076d3
Ramesh Lingappa <ramesh@rameshl.com>
2018-12-24 16:33:58 +0530

    npx card commit

--- build.js
+++ build.js
@@ -15,23 +15,23 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Vali Shah'),
-  handle: chalk.white('valishah'),
-  work: chalk.white('Senior Software Engineer - Fullstack'),
-  // opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('vali_shah'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~valishah'),
-  github: chalk.gray('https://github.com/') + chalk.green('valishah'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('valishah'),
-  // web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('valishah'),
+  name: chalk.white('       Ramesh Lingappa'),
+  handle: chalk.cyan('rameshl'),
+
+  work: chalk.white('Technical Architect | Full Stack Engineer'),
+  twitter:  chalk.cyan('https://twitter.com/ramesh_lingappa'),
+  github: chalk.cyan('https://github.com/ramesh-dev'),
+  linkedin: chalk.cyan('http://www.linkedin.com/in/rameshlingappa'),
+  medium: chalk.cyan('https://medium.com/@rameshlingappa'),
+  web: chalk.green('https://rameshl.com'),
+  npx: chalk.white('npx rameshl'),
+
   labelWork: chalk.white.bold('       Work:'),
-  // labelOpenSource: chalk.white.bold('Open Source:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
   labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  // labelWeb: chalk.white.bold('        Web:'),
+  labelMedium: chalk.white.bold('     Medium:'),
+  labelWeb: chalk.white.bold('        Web:'),
   labelCard: chalk.white.bold('       Card:')
 }
 
@@ -39,24 +39,23 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-// const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
-const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-// const webing = `${data.labelWeb}  ${data.web}`
+const mediuming = `${data.labelMedium}  ${data.medium}`
+
+const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-              //  opensourcing + newline + newline + // data.labelOpenSource + data.opensource
                twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
-              //  webing + newline + newline + // data.labelWeb + data.web
+               mediuming + newline + // data.labelMedium + data.medium
+               webing + newline + newline + // data.labelWeb + data.web
                carding // data.labelCard + data.npx
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
